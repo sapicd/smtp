f839f2abbe10d1503d2f1b122ac47f2abdb5b724
Hiroshi.tao <staugur@saintic.com>
2020-08-17 11:18:05 +0800

    update ui border

--- picbed_smtp.py
+++ picbed_smtp.py
@@ -9,7 +9,7 @@
     :license: BSD 3-Clause, see LICENSE for more details.
 """
 
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 __author__ = 'staugur <staugur@saintic.com>'
 __hookname__ = 'picbed-smtp'
 __description__ = '通过SMTP发送邮件'
@@ -19,33 +19,31 @@ from flask import g
 
 intpl_emailsetting = '''
 <div class="layui-col-xs12 layui-col-sm12 layui-col-md12">
-    <fieldset class="layui-elem-field">
+    <fieldset class="layui-elem-field layui-field-title">
         <legend>SMTP</legend>
         <div class="layui-field-box">
             <div class="layui-form-item">
-                <div class="layui-inline">
-                    <label class="layui-form-label" style="width: auto;">发件人</label>
-                    <div class="layui-input-inline">
-                        <input type="text" name="email_smtp_from"
-                            value="{{ g.site.email_smtp_from }}" placeholder="发件邮箱地址"
-                            autocomplete="off" class="layui-input">
-                    </div>
+                <label class="layui-form-label">发件人</label>
+                <div class="layui-input-block">
+                    <input type="text" name="email_smtp_from"
+                        value="{{ g.site.email_smtp_from }}" placeholder="发件邮箱地址"
+                        autocomplete="off" class="layui-input">
                 </div>
-                <div class="layui-inline">
-                    <label class="layui-form-label" style="width: auto;">密码</label>
-                    <div class="layui-input-inline">
-                        <input type="password" name="email_smtp_passwd"
-                            value="{{ g.site.email_smtp_passwd }}" placeholder="邮箱授权码或密码"
-                            autocomplete="off" class="layui-input">
-                    </div>
+            </div>
+            <div class="layui-form-item">
+                <label class="layui-form-label">密码</label>
+                <div class="layui-input-block">
+                    <input type="password" name="email_smtp_passwd"
+                        value="{{ g.site.email_smtp_passwd }}" placeholder="邮箱授权码或密码"
+                        autocomplete="off" class="layui-input">
                 </div>
-                <div class="layui-inline">
-                    <label class="layui-form-label" style="width: auto;">SMTP</label>
-                    <div class="layui-input-inline">
-                        <input type="text" name="email_smtp_server" id="render_email_server"
-                            value="{{ g.site.email_smtp_server }}" placeholder="SMTP服务器域名:端口(默认25)"
-                            autocomplete="off" class="layui-input">
-                    </div>
+            </div>
+            <div class="layui-form-item">
+                <label class="layui-form-label">SMTP</label>
+                <div class="layui-input-block">
+                    <input type="text" name="email_smtp_server" id="render_email_server"
+                        value="{{ g.site.email_smtp_server }}" placeholder="SMTP服务器域名:端口(默认25)"
+                        autocomplete="off" class="layui-input">
                 </div>
             </div>
         </div>
